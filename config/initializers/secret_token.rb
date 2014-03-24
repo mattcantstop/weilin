07357aaf051474b66c2c039092437379ad1a2fe5
Matt Duff <matt.duff@moneydesktop.com>
2014-03-24 08:07:38 -0600

    created user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weilin::Application.config.secret_key_base = 'aaa728b7448fe5611ee0f5f7fca8f0b5b85103f91a43320a4949923bfcb756f6b0035f963f2e0811a780bd8b39dc2f03e9e95a616a9e9a73989fc0d36f6279c4'
