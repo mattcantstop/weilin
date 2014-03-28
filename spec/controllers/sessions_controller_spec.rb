f449d7f948e8563a87f6417723c7478132b07a60
Matt Duff <matt.duff@moneydesktop.com>
2014-03-28 08:41:27 -0600

    mock is working now

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,16 +2,12 @@ require 'spec_helper'
 
 describe SessionsController, :type => :request do
 
-  before do
-    described_class.any_instance.stub(:user).and_return(user)
-  end
-
-  let!(:session_params_with_password) { { :email => user.email, :password => user.password } }
-  let!(:session_params_with_auth_token) { { :email => user.email, :authentication_token => user.authentication_token } }
+  let!(:user_with_password) { double('user', :password => "password") }
 
   describe "#create" do
-    it "lets request with a username/password through" do
-      post :create, sessions_params_with_password
+    it "lets request with a password through" do
+      binding.pry
+      post :create, :user_id => user_with_password, :password => user_with_password.password
       expect(response.status).to eq(201)
     end
   end
