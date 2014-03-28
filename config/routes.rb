fab5b7404a9281418618ad0c528587e20383c253
Matt Duff <matt.duff@moneydesktop.com>
2014-03-28 08:44:46 -0600

    added create method to sessions controller and routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Weilin::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
