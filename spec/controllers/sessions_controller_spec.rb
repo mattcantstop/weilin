478ad0a067723490238b9f37f696789bd949305d
Matthew Duff <matthewlduff@gmail.com>
2014-03-31 08:59:13 -0600

    added example for when no email is present

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -10,7 +10,12 @@ describe SessionsController, :type => :request do
       post :create, :user_id => user_with_password, :password => user_with_password.password
       expect(response.status).to eq(201)
     end
+
+    it "returns a 400 when the email is not present" do
+      ## Email not present example
+    end
   end
+
 # Need to add finding by email then authenticating with password or authentication_token
 
 end
