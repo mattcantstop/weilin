2bd35b94ef7a071f5300454e3c0946e63574fe0b
Matt Duff <matt.duff@moneydesktop.com>
2014-04-04 08:18:10 -0600

    functional test for wars create

--- app/controllers/wars_controller.rb
+++ app/controllers/wars_controller.rb
@@ -3,8 +3,14 @@ class WarsController < ApplicationController
   def create
     @war = War.new(war_params)
     if @war.save
-      render 'wars/show.rabl'
+      render 'wars/show.rabl', :status => 201
     end
+  end
+
+  private
+
+  def war_params
+    params.require(:war).permit(:name, :has_end, :ending_date, :ending_score, :prize, :is_private, :open_registration, :is_disabled)
   end
 
 end
