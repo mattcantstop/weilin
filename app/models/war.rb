ff3a375b2bf4cba15ba58f20f4e5301665cbe099
Matthew Duff <matthewlduff@gmail.com>
2014-04-08 08:19:36 -0600

    added participant association

--- app/models/war.rb
+++ app/models/war.rb
@@ -2,6 +2,7 @@ class War < ActiveRecord::Base
 
   has_many :battles
   has_many :participants
+  has_many :users, :through => :participants
   belongs_to :user, :foreign_key => :owner_id
   validates_presence_of :owner_id
 
